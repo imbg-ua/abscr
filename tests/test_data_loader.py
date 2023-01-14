415e02fe243d0d979fff9f62f8acdb75bbe3d4ca
YaroslavRy <ya.ryndyk@gmail.com>
2023-01-14 18:36:29 +0200

    wip

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -2,13 +2,17 @@ import unittest
 import sys
 sys.path.append('../')
 from abscr.abscr.dataload.data_loader import DataLoader
+from abscr.abscr.analysis.counter import CellCounter
 
 
 class TestDataloader(unittest.TestCase):
     def runTest(self):
         loader = DataLoader()
         self.assertEqual(loader.test_data_url, 'https://git.github.com/asdas/asda/test_example.pkl', 'URLs mismatch')
-        self.assertEqual(loader.load_test_data(), '200', 'Failed request')
+        self.assertEqual(loader.load_test_data().status_code, 200, 'Failed request')
+
+        cc = CellCounter()
+        self.assertEqual(cc.count_cells([]), -1, 'Cell counter does not work properly')
 
 
 unittest.main()
