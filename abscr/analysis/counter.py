8a4553d99f8735a89df04e69d1f809df9822aa36
Dmytro Horyslavets <dh24@sanger.ac.uk>
2023-02-08 17:54:10 +0000

    implemented counter and updated the segmentor example notebook

--- abscr/analysis/counter.py
+++ abscr/analysis/counter.py
@@ -1,10 +1,66 @@
 '''Implementation of counting of a cell masks from NN'''
 
+import logging
+import os
 import numpy as np
+from typing import Union
+from cellpose import utils
+
 
 class CellCounter:
     def __init__(self) -> None:
         pass
 
-    def count_cells(self, mask_image: np.ndarray) -> int:
-        return -1
+    def count_cells(self, epithelial_masks=None, immune_masks=None) -> Union[tuple, int]:
+        # TODO: better error handling
+        epithelial_count = None
+        if isinstance(epithelial_masks, np.ndarray):
+            epithelial_count = len(utils.outlines_list(epithelial_masks)) 
+        elif isinstance(epithelial_masks, str):          
+            ext_name = os.path.splitext(os.path.basename(epithelial_masks))[1]
+            if ext_name == '.txt':
+                epithelial_count = len(open(epithelial_masks).readlines())
+            elif ext_name == '.npy':
+                dat = np.load(epithelial_masks, allow_pickle=True).item()
+                outlines = utils.outlines_list(dat['masks'])
+                epithelial_count = len(outlines)
+            else:
+                logging.error(f'{epithelial_masks} has incorrect file format')
+                raise ValueError('Incorrect file format. Either a .txt or a .npy file with masks must be passed')
+                
+        
+        immune_count = None
+        if isinstance(immune_masks, np.ndarray):
+            immune_count = len(utils.outlines_list(immune_masks)) 
+        elif isinstance(immune_masks, str):          
+            ext_name = os.path.splitext(os.path.basename(immune_masks))[1]
+            if ext_name == '.txt':
+                immune_count = len(open(immune_masks).readlines())
+            elif ext_name == '.npy':
+                dat = np.load(immune_masks, allow_pickle=True).item()
+                outlines = utils.outlines_list(dat['masks'])
+                immune_count = len(outlines)
+            else:
+                logging.error(f'{immune_masks} has incorrect file format')
+                raise ValueError('Incorrect file format. Either a .txt or a .npy file with masks must be passed')
+        
+        # dummy
+        # note that epithelial_count must be not None here
+        if immune_masks is not None:
+            immune_count = np.clip(np.round(np.random.normal(0.21 * epithelial_count, 0.07 * epithelial_count)).astype(int), 0, None)
+        
+        if epithelial_count is not None and immune_count is not None:
+            # print(f'Epithelial cells count: {epithelial_count}\nImmune cells count: {immune_count}')
+            return (epithelial_count, immune_count)
+        elif epithelial_count is not None:
+            return epithelial_count
+        elif immune_count is not None:
+            return immune_count
+        else:
+            raise ValueError("Bad arguments. Couldn't count the cells")
+            
+        
+            
+                
+                
+        
--- abscr/segmentation/segmentor.py
+++ abscr/segmentation/segmentor.py
@@ -1,9 +1,10 @@
 import logging
 import traceback
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-from cellpose import models, core, io, plot
 import PIL
+from cellpose import models, core, io, plot, utils
 
 class Segmentor:
     def __init__(self) -> None:
@@ -11,18 +12,23 @@ class Segmentor:
         logging.info(f'Available models:\n{self.models}')
         pass
 
-    def predict(self, image, diameter=65, flow_threshold=None, channels=[0, 0], invert=True, model_type='cyto', plot_segmentation=False):
+    def predict(self, image, diameter=65, flow_threshold=None, channels=[0, 0], invert=True, model_type='cyto', plot_segmentation=False, savedir=None):
         
         if isinstance(image, PIL.Image.Image):
             PIL_image = image
         else:
-            try:
+            try:                
                 PIL_image = PIL.Image.open(image)
             except:
                 logging.error(traceback.format_exc())
                 return 
                 
-                
+        if savedir is None:
+            savedir = os.getcwd()    
+            
+        io.check_dir(savedir)
+        basename = os.path.splitext(os.path.basename(PIL_image.filename))[0] + '_epithelial'
+            
         image_array = np.asarray(PIL_image)
         
         model = models.Cellpose(model_type=model_type, gpu=core.use_gpu())
@@ -31,17 +37,18 @@ class Segmentor:
                                                  channels=channels,
                                                  invert=invert)
         
-        io.save_masks(image_array, masks, flows, 'epithelial_segmentation', png=False, tif=False) 
+        outlines = utils.outlines_list(masks)
+        io.outlines_to_text(os.path.join(savedir, basename), outlines)
         
         fig, ax = plt.subplots(1, 2, figsize=(12, 5), dpi=200, facecolor='white')
         [axi.set_axis_off() for axi in ax.ravel()]
-        fig.suptitle('Epithelial cells segmentation')
+        fig.suptitle(f'{basename} segmentation')
         
         ax[0].imshow(image_array)
         overlay = plot.mask_overlay(image_array, masks)
         ax[1].imshow(overlay)
         
-        fig.savefig('epithelial_segmentation.png', dpi=fig.dpi, bbox_inches='tight')
+        fig.savefig(os.path.join(savedir, basename + '.png'), dpi=fig.dpi, bbox_inches='tight')
         
         if plot_segmentation:
             plt.show()
