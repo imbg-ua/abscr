422c2bf83a8ec9a2078989bc0e212634eef1b4a3
Yaroslav <ya.ryndyk@gmail.com>
2023-03-10 11:50:31 +0200

    host and port set explicitly

--- annotation_tool/app.py
+++ annotation_tool/app.py
@@ -506,4 +506,6 @@ def update_download_link(data, columns):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    host = '0.0.0.0'
+    port = 8050
+    app.run_server(debug=True, host=host, port=port)
